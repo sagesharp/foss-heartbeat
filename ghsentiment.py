4f9572b6346037ba74da78f9010e3659ea707780
Sarah Sharp <sharp@otter.technology>
2016-11-29 22:45:49 -0800

    Sentiment: More options for directing which json files to parse.
    Sometimes you want to get scrubbed comments for an entire git repo.
    Sometimes you just want scrubbed comments for a particular file or
    issue. I've added options for both to ghsentiment.py.
    
    The command line format is kind of wonky, but I'm just trying to get
    results right now, and I'll fix this technical debt later.
    
    Signed-off-by: Sarah Sharp <sharp@otter.technology>

--- ghsentiment.py
+++ ghsentiment.py
@@ -38,100 +38,152 @@ import json
 import emoji
 import string
 
+def scrubFile(f):
+    with open(f) as jsonFile:
+        try:
+            soup = json.load(jsonFile)
+        except:
+            return None
+    text = soup.get('body')
+    if not text:
+        return None
+
+    # Standardize on unix line endings.  (Yes, for whatever reason some
+    # projects use Windows line endings, some unix).
+    text = text.replace('\r\n', '\n')
+      
+    # Strip out any blockquotes or inline code
+    text = re.sub('```.+?```', 'block-code.', text, flags=re.DOTALL)
+    text = re.sub('`[^`]+`', 'inline-code', text, flags=re.MULTILINE)
+    text = re.sub('^    .+?$', 'block-code.', text, flags=re.MULTILINE)
+    # Remove any quoted text, since we want the sentiment of the person posting
+    text = re.sub('^>.+?$', '', text, flags=re.MULTILINE)
+    # Replace any URLs with their text
+    text = re.sub('\[(.*?)\]\(.*?\)', '\g<1>', text)
+    text = re.sub('https?:.+? ', 'URL ', text)
+    text = re.sub('https?:.+?$', 'URL.', text, flags=re.MULTILINE)
+    # convert emojis into their short hand code.
+    # This makes it easier for me to correct sentiment in the training text.
+    # It also allows the Standford CoreNLP to parse each emoji as a separate word,
+    # which will allow us to train it for sentiment of groups of emoji.
+    # E.g. :tea: is neutral, but :tea: :fire: references the "This is fine" meme
+    text = emoji.demojize(text)
+    # We often have blocks of code follow a colon, e.g.
+    #
+    # This is not correct syntax for python 3:
+    # ```print foo```
+    # This is the correct syntax:
+    # ```print(foo)```
+    #
+    # This code will translate that into
+    #
+    # This is not correct syntax for python 3:
+    # block-quote
+    # This is the correct syntax:
+    # block-quote
+    #
+    # The Standford CoreNLP assumes that the sentence continues
+    # after the colon, because it's assuming sentences like
+    # "Henry is the proper gentleman: charming, polite, and classy."
+    #
+    # We really want to consider lines that end with a : as a sentence.
+    # For lines that end with an emoji, we want to add a '.' at the end.
+    # Compromise and just add a '.' at the end of both.
+    text = re.sub(':$', ':.', text, flags=re.MULTILINE)
+
+    # FIXME: Standford CoreNLP doesn't parse '...' as the end of a sentence.
+    # If '...' is at the end of a line, turn it into '.'
+    # Does the text end with punctuation? If not, add a '.'
+
+    # FIXME: ugh, no idea what to do with sentences that end in :)
+
+    # FIXME: some people don't put periods after @tag someone.
+
+    # FIXME: Ignore comments from any bots? They tend to not use punctuation.
+
+    # FIXME: Ignore commands sent to bots
+
+    # We can find when they do this at the end of the line.
+    if text.split('\n')[-1][-1:] not in string.punctuation:
+        text = text + '.'
+    return text
+
 # Create a raw text file for the issue in question
 # Lines with a # are used to denote which json file the words came from
 def scrubText(repoPath, issueDir):
     scrubbed = ""
     files = os.listdir(os.path.join(repoPath, issueDir))
     for f in files:
-            with open(os.path.join(repoPath, issueDir, f)) as jsonFile:
-                soup = json.load(jsonFile)
-            text = soup.get('body')
-            if not text:
-                continue
-
-            # Standardize on unix line endings.  (Yes, for whatever reason some
-            # projects use Windows line endings, some unix).
-            text = text.replace('\r\n', '\n')
-
-            # FIXME: insert a '.\n' before the file name to make sure dangling
-            # sentences from the last issue don't get combined with this one
-            scrubbed = scrubbed + '#' + os.path.join(issueDir, f) + '\n'
-            # Strip out any blockquotes or inline code
-            text = re.sub('```.+?```', 'block-code.', text, flags=re.DOTALL)
-            text = re.sub('`[^`]+`', 'inline-code', text, flags=re.MULTILINE)
-            text = re.sub('^    .+?$', 'block-code.', text, flags=re.MULTILINE)
-            # Remove any quoted text, since we want the sentiment of the person posting
-            text = re.sub('^>.+?$', '', text, flags=re.MULTILINE)
-            # Replace any URLs with their text
-            text = re.sub('\[(.*?)\]\(.*?\)', '\g<1>', text)
-            text = re.sub('https?:.+? ', 'URL ', text)
-            text = re.sub('https?:.+?$', 'URL.', text, flags=re.MULTILINE)
-            # convert emojis into their short hand code.
-            # This makes it easier for me to correct sentiment in the training text.
-            # It also allows the Standford CoreNLP to parse each emoji as a separate word,
-            # which will allow us to train it for sentiment of groups of emoji.
-            # E.g. :tea: is neutral, but :tea: :fire: references the "This is fine" meme
-            text = emoji.demojize(text)
-            # We often have blocks of code follow a colon, e.g.
-            #
-            # This is not correct syntax for python 3:
-            # ```print foo```
-            # This is the correct syntax:
-            # ```print(foo)```
-            #
-            # This code will translate that into
-            #
-            # This is not correct syntax for python 3:
-            # block-quote
-            # This is the correct syntax:
-            # block-quote
-            #
-            # The Standford CoreNLP assumes that the sentence continues
-            # after the colon, because it's assuming sentences like
-            # "Henry is the proper gentleman: charming, polite, and classy."
-            #
-            # We really want to consider lines that end with a : as a sentence.
-            # For lines that end with an emoji, we want to add a '.' at the end.
-            # Compromise and just add a '.' at the end of both.
-            text = re.sub(':$', ':.', text, flags=re.MULTILINE)
-
-            # FIXME: Standford CoreNLP doesn't parse '...' as the end of a sentence.
-            # If '...' is at the end of a line, turn it into '.'
-            # Does the text end with punctuation? If not, add a '.'
-
-            # FIXME: ugh, no idea what to do with sentences that end in :)
-
-            # FIXME: some people don't put periods after @tag someone.
-
-            # FIXME: Ignore comments from any bots? They tend to not use punctuation.
-
-            # FIXME: Ignore commands sent to bots
-
-            # We can find when they do this at the end of the line.
-            if text.split('\n')[-1][-1:] not in string.punctuation:
-                text = text + '.'
-            scrubbed = scrubbed + text + '\n'
+        scrubbed = scrubbed + '#' + os.path.join(issueDir, f) + '\n'
+        text = scrubFile(os.path.join(repoPath, issueDir, f))
+        if text:
+            scrubbed = scrubbed + text + '\n.\n'
     return scrubbed
 
+def findJsonFiles(matchedFile, searchDirs):
+    matchedName, matchedExt = os.path.splitext(matchedFile)
+    if not matchedExt == '.json':
+        return None
+    if not searchDirs:
+        jsonFiles = [matchedFile]
+    else:
+        baseDir = os.path.dirname(matchedFile)
+        try:
+            jsonFiles = [os.path.join(baseDir, x) for x in os.listdir(baseDir)]
+        except:
+            print("WARN: Error grabbing file", matchedFile, "basedir:", baseDir)
+            return None
+    return jsonFiles
+
+def findRepoJsonFiles(repoPath):
+    issues = [os.path.join(repoPath, x) for x in os.listdir(repoPath)
+              if x.startswith('issue-') and os.path.isdir(os.path.join(repoPath, x))]
+    jsonFiles = []
+    for i in issues:
+        jsonFiles = jsonFiles + [os.path.join(i, x) for x in os.listdir(i)]
+    return jsonFiles
+
+# File format is relative path to json file (starting with owner/repo), one per line
 def main():
     parser = argparse.ArgumentParser(description='Generate scrubbed conversation to feed into sentiment analysis')
-    parser.add_argument('repository', help='github repository name')
-    parser.add_argument('owner', help='github username of repository owner')
-    parser.add_argument('file', help='file to store processed comments')
-    parser.add_argument('num', type=int, help='number of issues to process. 0 means all')
+    parser.add_argument('inFile', help='input file containing paths to json files, or (with --recurse) path to repo directory')
+    parser.add_argument('outFile', help='output file')
+    parser.add_argument("--dirs", help="inFile contains one or more paths to a json file; output all comments in the issue related to the given json file",
+                        action="store_true", default=False)
+    parser.add_argument("--recurse", help="inFile is a path to a repo; output all comments on all issues",
+                        action="store_true", default=False)
     args = parser.parse_args()
 
-    repoPath = os.path.join(args.owner, args.repository)
-    issues = [x for x in os.listdir(repoPath)
-              if x.startswith('issue-') and os.path.isdir(os.path.join(repoPath, x))]
-    with open(args.file, 'w') as commentFile:
-        for index, i in enumerate(issues):
-            commentFile.write(scrubText(repoPath, i))
-            if (index % 1000 == 0):
-                print("Processed", index, "issues")
-            if int(args.num) > 0 and index >= int(args.num):
-                break
+    # FIXME: I think there's probably a way to make the flags exclusive?
+    if args.dirs and args.recurse:
+        print("Either you want to parse a file with a specific list of json paths,")
+        print("or you want to parse all json files for a repo. You cannot do both.")
+        return
+
+    if not args.recurse:
+        with open(args.inFile) as f:
+            paths = f.read().split('\n')
+    else:
+        paths = [args.inFile]
+
+    jsonCount = 0
+    with open(args.outFile, 'w') as commentFile:
+        for line in paths:
+            if args.recurse:
+                jsonFiles = findRepoJsonFiles(line)
+            else:
+                jsonFiles = findJsonFiles(line, args.dirs)
+            if not jsonFiles:
+                continue
+            for json in jsonFiles:
+                text = scrubFile(json)
+                jsonCount = jsonCount + 1
+                if text:
+                    commentFile.write('#' + json + '\n')
+                    commentFile.write(text)
+                    commentFile.write('\n.\n')
+                if (jsonCount != 0 and jsonCount % 5000 == 0):
+                    print("Processed", jsonCount, "json files")
 
 if __name__ == "__main__":
     main()
